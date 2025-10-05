bd86e567107d7edca51990b13bd19fcf0d8544be
saeid-shariati <shariati1987@gmail.com>
2025-10-05 23:01:04 +0330

    Add project files.

---
+++ BookShopping.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShopping.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
