6272d5de0d6aa15827a36c3cbed4d9dd2d2f183d
saeid-shariati <shariati1987@gmail.com>
2025-10-08 00:16:25 +0330

    add class implementation to entities

--- BookShopping.Domain/Entities/ProductImage.cs
+++ BookShopping.Domain/Entities/ProductImage.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,14 @@ namespace BookShopping.Domain.Entities
 {
     public class ProductImage
     {
+        [Key]
+        public Guid ProductImageId { get; set; }
+
+        public string ImageUrl { get; set; }
+        public Guid ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
     }
 }
--- BookShopping.Infrastructure/Data/ApplicationDbContext.cs
+++ BookShopping.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookShopping.Domain.Entities;
+using BookShopping.Domain.Entities.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookShopping.Infrastructure.Data
@@ -9,5 +12,32 @@ namespace BookShopping.Infrastructure.Data
             : base(options)
         {
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        //public DbSet<Company> Companies { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUserLogin<Guid>>(entity =>
+            {
+                entity.HasKey(login => new { login.ProviderKey, login.LoginProvider });
+            });
+            builder.Entity<IdentityUserRole<Guid>>(entity =>
+            {
+                entity.HasKey(role => new { role.UserId, role.RoleId });
+            });
+            builder.Entity<IdentityUserClaim<Guid>>(entity =>
+            {
+                entity.HasKey(claim => claim.Id);
+            });
+        }
+
     }
 }
